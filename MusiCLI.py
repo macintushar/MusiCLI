ef0dcf6f140d2e6a1b368a7fd6b40fb1360fe1b5
Tushar Selvakumar <tusharkumar91111@gmail.com>
2023-10-17 10:11:20 +0530

    Added Caching

--- MusiCLI.py
+++ MusiCLI.py
@@ -1,8 +1,9 @@
-import os
+import os, pygame
 import random as rnd
-import pygame
+import pandas as pd
 
 CONFIG_FILE = "musicli-config.txt"
+CACHE_FILE = "musicli-cache.csv"
 
 def get_albums(path):
     albums = {}
@@ -32,8 +33,7 @@ def SequentialSearch(term,path):
         print("Matching albums are: ", matches)
         return matches
 
-def PlayRandomSong():
-    album_dict = get_albums(path)
+def PlayRandomSong(album_dict):
     all_songs = list(album_dict.items())
     
     total_albums = len(all_songs)
@@ -46,7 +46,7 @@ def PlayRandomSong():
 
     random_song_path = random_song['path']
     
-    return random_song['file'],random_song_path
+    return random_song['file'], random_song_path, random_album[0]
 
 def Get_Album_Songs(album_name,path):
     all_albums = get_albums(path)
@@ -59,14 +59,54 @@ def search_songs(songs, query):
             results.append(song)
     return results
 
+def search_albums(albums, query):
+    results = []
+    for album in albums:
+        if query.lower() in album.lower():
+            results.append(album)
+    return results
+
+
+def createCSVCache(albums):
+    album_names = list(albums.keys())
+    all_files = []
+    for al in album_names:
+        for so in albums[al]:
+            all_files.append([al,so["file"], so["path"]])
+
+    df = pd.DataFrame(all_files, columns=["Album","File","Path"])
+    df.to_csv(CACHE_FILE,index = False)
+    print("Data cached")
+
+def readCSVCache(cache_path):
+    musicDF = pd.read_csv(cache_path)
+    print("Reading Cache")
+    albums = musicDF["Album"]
+    albums = list(albums.unique())
+    vals = musicDF.to_numpy()
+
+    album_dict = {}
+
+    for i in albums:
+        all_songs = []
+        for j in range(0, len(vals)):
+            song = {}
+            if vals[j][0] == i:
+                song["file"] = vals[j][1]
+                song["path"] = vals[j][2]
+                all_songs.append(song)
+        album_dict[i] = all_songs
+    
+    return album_dict
+
 def setup():
     path = input("Enter the path to your music folder: ")
     with open(CONFIG_FILE, "w") as config_file:
         config_file.write(path)
 
 class Player:
-    def Play(path, file):
-        print("Now Playing:", file)
+    def Play(path, file, album = ""):
+        print("Now Playing:", file, "from: ", album)
         pygame.mixer.init()
         pygame.mixer.music.load(path)
         pygame.mixer.music.play()
@@ -79,9 +119,9 @@ class Player:
         print("Unpaused song:", file)
         pygame.mixer.music.unpause()
     
-    def ChangeSong(path,file):
+    def ChangeSong(path,file, album):
         print("Changing song")
-        Player.Play(path,file)
+        Player.Play(path, file, album)
 
     def Stop():
         pygame.mixer.music.stop()
@@ -92,95 +132,91 @@ def main():
 
     with open(CONFIG_FILE, "r") as config_file:
         path = config_file.read().strip()
+    
+    if os.path.exists(CACHE_FILE):   
+        album_dict = readCSVCache(CACHE_FILE) 
 
-    music_folder = path
-    album_dict = get_albums(music_folder)
+    if not os.path.exists(CACHE_FILE):    
+        music_folder = path
+        album_dict = get_albums(music_folder)
+        createCSVCache(album_dict)
 
     print("""
-                            ..                     :-=========--:               
-                    .:-=+#%@@@%                    :+- .-==========.             
-            -=+#%%@@@@@%#++-*@%                    -+=::========--=:             
-            .@@#+=-:.....:-=+%@%                    :------:-====--=:             
-            .@@=:=+*#%@@@@@%#%@%              :-====---------==-----.=%###*:      
-            .@@@@%#*+=-:.    =@%            :+++=============------=.=@%%%@%-     
-            .@@:             +@%            ==================------.*%%%%%%#     
-            .@@:             =@%           .==========----:::::::::-*%%%%%%%%.    
-            @@:       .+%@@%%@%           .========::+***********#%%%%%%%%%%:    
-        :*#%#*@@:      -@@*--+@@%            =======:-%@@@@@@@@%%%%%%%%%%%%%%#     
-    *@@*=*@@@:      *@%    *@%            :======.*@%%%%%%%%%%%%%%%%%%%%%%-     
-    :@@:   :@@:      .%@@**%@%-             .----=.*@%%%%%%%#############*:      
-    *@@*=+@@#         -+*#*-                      *%%%%%%%#+++**++-             
-        -*%%%*-                                      *%%%%%%%%%%#==#%+             
-                                                    =%%%%%%%%%%*..*%-             
-                                                    :+*#%%%%%%%#*=.              
-                                                        ..:::..                                   
+                    .:-=+#%@@@%               
+            -=+#%%@@@@@%#++-*@%  
+            .@@#+=-:.....:-=+%@% 
+            .@@=:=+*#%@@@@@%#%@% 
+            .@@@@%#*+=-:.    =@% 
+            .@@:             +@% 
+            .@@:             =@% 
+            @@:       .+%@@%%@%  
+        :*#%#*@@:      -@@*--+@@%
+    *@@*=*@@@:      *@%    *@%   
+    :@@:   :@@:      .%@@**%@%-  
+    *@@*=+@@#         -+*#*-     
+        -*%%%*-                  
     """)
 
     while True:
         print("\n\n=================================================================================================")
-        print("======================= Welcome to SpotiPy - your python-cli music player =======================")
+        print("======================= Welcome to MusiCLi - your python-cli music player =======================")
         print("=================================================================================================")
 
         print("\n1. Search for an album")
         print("2. Search for a song")
         print("3. Play a random song")
+        print("4. Refresh Cache")
         print("9. Exit")
         opt = input("Enter your option: ")
 
         if opt == "1":
-            print()
-            searchTerm = input("Enter your search term: ")
-            searched_albums = SequentialSearch(searchTerm, music_folder)
-
-            if searched_albums:
-                to_play_album = input("Do you want to play the Album? (Y/N): ")
-
-                if to_play_album == "Y" or to_play_album == "y":
-
-                    play_queue = Get_Album_Songs(searched_albums[0],music_folder)
-                    
-                    for i in play_queue:
-
-                            path = i['path']
-                            file = i['file']
-
-                            Player.Play(path,file)
-                            
-                            while True:
-                                player_option = int(input("\n(1)Pause (2)Play (3)Stop (4)Next Song: "))
-
-                                if player_option == 1:
-                                    Player.Pause(path,file)
-                                
-                                if player_option == 2:
-                                    Player.Unpause(path,file)
-                                
-                                if player_option == 3:
-                                    print("Stopping playback. Returning to Main Menu.")
-                                    break
-
-                                if player_option == 4:
-                                    Player.Stop()
-                                    break
-                            if player_option == 3:
-                                    break
-        
-        if opt == "2":
-            albums = get_albums("/home/tushar/Documents/SoulSeek/SeekShare/Music")
-
             songs = []
 
-            for i in albums:
-                for j in albums[i]:
-                    #print(j)
+            for i in album_dict:
+                for j in album_dict[i]:
                     songs.append(j)
 
+            query = input("Enter an album to search for: ")
+            res = search_albums(albums=list(album_dict.keys()), query=query)
+            
+            if len(res)>0:
+                print('\nAlbums that match your search term "'+query+'" are:')
+                for i in range(len(res)):
+                    print(str(i)+":",res[i]['file'])
 
-            #print(songs)
-            print()
+                choice = int(input("What album to play? :"))
+            
+            else:
+                print("No songs match your search term '"+query+"'")
+                continue
+
+            path = res[choice]['path'] 
+            file = res[choice]['file']
+
+            Player.Play(path,file)
+
+            while True:
+                player_option = int(input("\n(1)Pause (2)Play (3)Stop: "))
+
+                if player_option == 1:
+                    Player.Pause(path,file)
+                
+                if player_option == 2:
+                    Player.Unpause(path,file)
+                
+                if player_option == 3:
+                    Player.Stop()
+                    break
+        
+        if opt == "2":
+            songs = []
+
+            for i in album_dict:
+                for j in album_dict[i]:
+                    songs.append(j)
+
             query = input("Enter a song to search for: ")
             res = search_songs(songs=songs,query=query)
-            #print(res)
             
             if len(res)>0:
                 print('\nSongs that match your search term "'+query+'":')
@@ -199,7 +235,7 @@ def main():
             Player.Play(path,file)
 
             while True:
-                player_option = int(input("\n(1)Pause (2)Play (3)Stop (4)Change Song: "))
+                player_option = int(input("\n(1)Pause (2)Play (3)Stop: "))
 
                 if player_option == 1:
                     Player.Pause(path,file)
@@ -214,9 +250,9 @@ def main():
         if opt == "3":
             print()
             print("\nPlaying a random song..")
-            file,path = PlayRandomSong()
+            file,path, album = PlayRandomSong(music_folder)
             
-            Player.Play(path,file)
+            Player.Play(path,file, album)
             
             while True:
                 player_option = int(input("\n(1)Pause (2)Play (3)Stop (4)Change Song: "))
@@ -232,9 +268,11 @@ def main():
                     break
 
                 if player_option == 4:
-                    file,path = PlayRandomSong()
-                    Player.ChangeSong(path,file)
-            
+                    file,path, album = PlayRandomSong(music_folder)
+                    Player.ChangeSong(path, file, album)
+        
+        if opt == "4":
+            createCSVCache(album_dict)
 
         if opt == 'dict':
             print(album_dict)
